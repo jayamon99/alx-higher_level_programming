92877a0cb12d0823e45899c38391bcb4ae626c68
jayamon99 <henryquaye99@outlook.com>
2022-09-05 23:39:10 +0000

    update

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = require("node:process");
+
+function add(a, b) {
+  a = parseInt(argv[2]);
+  b = parseInt(argv[3]);
+  return a + b;
+}
+if (argv.length <= 3) {
+  console.log(NaN);
+} else {
+  console.log(add(argv[2], argv[3]));
+}
